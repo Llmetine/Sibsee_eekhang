a07991fd1d5ae7c7da6e664b2046f103ac0bfe3e
Llmetine <ntjeddelog@gmail.com>
2025-01-28 09:45:10 +0700

    finished

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	int x;
+	for(int i=0;i<N-1;i++){
+		for(int j=i;j>=0;j--){
+			if(d[j]<d[j+1]){
+			x=d[j];
+			d[j]=d[j+1];
+			d[j+1]=x;
+			}
+		}
+		cout <<"Pass "<<i+1<<":";
+		for (int j=0;j<N;j++){
+		cout <<d[j]<< " ";
+		}
+		cout <<endl;
+	}
 }
 
 int main(){
